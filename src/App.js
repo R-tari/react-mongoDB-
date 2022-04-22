c9ccbe9d113f6b197ae5c748acfa1f37425789bb
R-tari <95198393+R-tari@users.noreply.github.com>
2022-04-22 16:27:56 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,19 +12,18 @@ function Header()
     <div className="container">
 
       <h1>
-        Playrist
+        <Link to="/">
+          Playrist
+          </Link>
       </h1>
-        <Nav variant="pills" defaultActiveKey="/home">
+        <Nav variant="pills">
           <Nav.Item>
-            <Nav.Link href="/home">재생목록</Nav.Link>
+            <Nav.Link href="/playlist">재생목록</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">음악추가</Nav.Link>
+            <Nav.Link eventKey="link-1" href="/write" >음악추가</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
-            </Nav.Link>
           </Nav.Item>
         </Nav>
     </div>
@@ -40,19 +39,86 @@ function SecContent(props)
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
-                <th>{id}</th>
-                <th>{title}</th>
-                <th>{artist}</th>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Artist</th>
                
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>1</td>
-                <td>Mark</td>
+                <td>pompeii</td>
                 <td className="Art">
-                  Otto
-                  <CloseButton variant="white" />
+                    bastille
+                  
+                </td>
+              </tr>
+
+              <tr>
+                <td>2</td>
+                <td>Why Do You Love Me</td>
+                <td className="Art">
+                Charlotte Lawrence
+                  
+                </td>
+              </tr>
+
+              <tr>
+                <td>3</td>
+                <td>Lonely</td>
+                <td className="Art">
+                Imagine Dragon
+                  
+                </td>
+                </tr>
+
+
+                <tr>
+                <td>4</td>
+                <td>창귀</td>
+                <td className="Art">
+                  안예은
+                  
+                </td>
+                </tr>
+
+
+
+                <tr>
+                <td>5</td>
+                <td>고백</td>
+                <td className="Art">
+                  델리스파이스
+                  
+                </td>
+                </tr>
+              
+              <tr>
+                <td>6</td>
+                <td>잊혀진 계절</td>
+                <td className="Art">
+                  이용
+                 
+                </td>
+              </tr>
+
+
+              <tr>
+                <td>7</td>
+                <td>아이스크림 사랑</td>
+                <td className="Art">
+                  임병수
+                 
+                </td>
+                </tr>
+                <tr>
+
+                <td>8</td>
+                <td>그대에게</td>
+                <td className="Art">
+                  무한궤도
+                  
                 </td>
                 
               </tr>
@@ -65,14 +131,42 @@ function SecContent(props)
   )
 }
 
+function Index()
+{
+  <h2>Main Page</h2>
+}
+
+function Addmusic()
+{
+  return(
+    <Container fluid="md">
+      <div className="write">
+        <form action="/add" method="post">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Title</label>
+            <input name="title" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />          
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Artist</label>
+            <input name="artist" type="text" class="form-control" id="exampleInputPassword1" />          
+          </div>        
+          <button type="submit" class="btn btn-primary">Add</button>
+        </form>
+      </div>
+    </Container>
+  )
+}
+
 
 
 
 function Footer()
 {
-  <address>
-    이 페이지는 학습목적으로 제작되었으며 영리목적이 없음을 알립니다.
-  </address>
+  return(
+    <address>
+      이 페이지는 학습목적으로 제작되었으며 영리목적이 없음을 알립니다.
+    </address>
+  )
 }
 
 
@@ -83,12 +177,14 @@ function App() {
       <div className="App">
         <Header/>
         <Routes>
-          <Route  path="/" element={<SecContent id="1" title="잊혀진 계절" artist="이용" />}/>
+          <Route path="/" element={<Index></Index>}></Route>
+          <Route  path="/write" element={<Addmusic/>}/>
         </Routes>
         <Footer/>
       </div>
     </BrowserRouter>
   );
 }
+
 
 export default App;
